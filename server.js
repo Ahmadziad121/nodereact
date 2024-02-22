1881c47e8cbb6fbf50791f5c44d353c91a157bf1
Ahmadziad <158805229+Ahmadziad121@users.noreply.github.com>
2024-02-22 19:55:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ app.get("/getprices",function(req,res){
     res.json({prise1:"10",prise1:"20",prise3:"40",prise4:"50"})
 
 })
+var server =app.listen(1000,function()
+
+{
+    var host =server.address().address
+    var port =server.address().port
+})
+
 
 
 
